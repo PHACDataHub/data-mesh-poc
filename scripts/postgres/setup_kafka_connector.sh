24a86e71d7dc6204e8cc597b7c43bed3a94e5f62
Nghia Doan <nghia.doan@phac-aspc.gc.ca>
2022-12-21 18:45:35 +0000

    Added Kafka Streams.

--- scripts/postgres/setup_kafka_connector.sh
+++ scripts/postgres/setup_kafka_connector.sh
@@ -26,3 +26,26 @@ curl -s "http://"${connect}"/connectors?expand=info&expand=status" | \
 	jq '. | to_entries[] | [ .value.info.type, .key, .value.status.connector.state, .value.status.tasks[].state, .value.info.config."connector.class" ] | join (":|:")' | \
 	column -s : -t | sed 's/\"//g' |sort 
     
+topic_name=CALIFORNIA_COVID
+composite_keys=DATE,FIPS
+
+curl -X PUT http://${connect}/connectors/sink-california-covid/config \
+    -H "Content-Type: application/json" \
+    -d '{
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+        "connection.url": "jdbc:postgresql://'${postgres}'/",
+        "connection.user": "postgres",
+        "connection.password": "postgres",
+        "tasks.max": "1",
+        "topics": "'${topic_name}'",
+        "auto.create": "true",
+        "auto.evolve": "true",
+        "pk.mode": "record_value",
+        "pk.fields":"'${composite_keys}'",
+        "insert.mode": "upsert",
+        "table.name.format":"'${topic_name}'"
+    }'
+
+curl -s "http://"${connect}"/connectors?expand=info&expand=status" | \
+	jq '. | to_entries[] | [ .value.info.type, .key, .value.status.connector.state, .value.status.tasks[].state, .value.info.config."connector.class" ] | join (":|:")' | \
+	column -s : -t | sed 's/\"//g' |sort 
